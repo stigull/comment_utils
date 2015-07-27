5e4790175047180ed9604f76078b74d5f6fa5d66
hertogi <hertogi@sigma.rhi.hi.is>
2015-07-27 11:57:33 +0000

    Code change cleanup on server update

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
-from comment_utils.views import post_comment
+from comment_utils.views import post_comment, no_comment
 
 urlpatterns = patterns('',
+    url(r'^post/$', no_comment, name = 'no_comment'),
     url(r'^skra-athugasemd/$', post_comment, name = 'post_comment'),
 ) 
--- views.py
+++ views.py
@@ -6,27 +6,43 @@ from django.template.loader import render_to_string
 
 from utils.djangoutils import JSONResponse
 
+def no_comment(request):
+    return JSONResponse(object = {})
+
 def post_comment(request):
     jsonobject = {}
     if request.method == "POST":
-        django_post_comment(request) #TODO: Better validation!
         data = request.POST.copy()
+        verification_code = data.get("verification")
+        object_pk = data.get("object_pk")
+        verified = False
+        try:
+            #Uppfaert. Spurning geymd i stigull/templates/comments/forms.html
+            if verification_code == 'geirfugl':
+                verified = True
+        except:
+            verified = False
+        
+        if not verified:
+            jsonobject['succeeded'] = False
+            return JSONResponse(object = jsonobject)
+        
+        django_post_comment(request) #TODO: Better validation!
         
         ctype = data.get("content_type")
-        object_pk = data.get("object_pk")
-
+        
         if ctype is None or object_pk is None:
-            jsonobject['succeeded'] = False 
+            jsonobject['succeeded'] = False
         else:
             try:
                 model = models.get_model(*ctype.split(".", 1))
                 object = model._default_manager.get(pk=object_pk)
             except TypeError:
-                jsonobject['succeeded'] = False 
+                jsonobject['succeeded'] = False
             else:
                 jsonobject['succeeded'] = True
                 jsonobject['comments'] = render_to_string('comments/list_of_comments.html', {'object': object, 'user': request.user })
     else:
-        jsonobject['succeeded'] = False        
+        jsonobject['succeeded'] = False
+    
     return JSONResponse(object = jsonobject)
-            
